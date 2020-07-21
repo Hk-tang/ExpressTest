040615bf4958f0b0e9baf0a306cecdc1f8026be1
Henry Tang <hktang@ualberta.ca>
2020-07-21 10:31:08 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/listUsers", (req, res, next) => {
+    fs.readFile(__dirname + "/" + "users.json", "utf8", (err, data) => {
+        console.log(data);
+        res.end(data);
+    })
+})
+
+app.get('/:id', function (req, res, next) {
+    fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
+        const users = JSON.parse(data);
+        const user = users["user" + req.params.id]
+        console.log(user);
+        res.end(JSON.stringify(user));
+    })
+})
+
+const server = app.listen(3000, () => {
+    const host = server.address().address
+    const port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port);
+});
